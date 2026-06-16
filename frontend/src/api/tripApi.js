c23e0c6eb9da14a31a873259f6e6e21bec2bcdeb
michellee15 <e1459250@u.nus.edu>
2026-06-16 11:39:17 +0800

    Add all CRUD functions into frontend tripApi.js

--- frontend/src/api/tripApi.js
+++ frontend/src/api/tripApi.js
@@ -8,7 +8,22 @@ export const getTrips = async () => {
   return response.data;
 };
 
+export const getTripById = async (id) => {
+  const response = await axios.get(`${API_URL}/${id}`);
+  return response.data;
+};
+
 export const createTrip = async (tripData) => {
   const response = await axios.post(API_URL, tripData);
   return response.data;
 };
+
+export const updateTrip = async (id, tripData) => {
+  const response = await axios.put(`${API_URL}/${id}`, tripData);
+  return response.data;
+};
+
+export const deleteTrip = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+  return response.data;
+}
