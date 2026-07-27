ecd7b4a9266e0df1b2d0714ac1e14a930b10607f
michellee15 <e1459250@u.nus.edu>
2026-07-27 20:03:36 +0800

    Improve navigation of page to allow user to restore the previous landed page once logged in

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 function ProtectedRoute({children}) {
   const token = localStorage.getItem("token");
-  if (!token) return <Navigate to="/login" replace/>;
+  const location = useLocation();
+  if (!token) return <Navigate to="/login" replace state={{from: location}}/>;
   return children;
 }
 
